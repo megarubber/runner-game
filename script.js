f682c3cdc2cd675554d9ef57bc508d14e15f1f0c
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-01 22:27:16 -0300

    feat - functions and config index

---
+++ script.js
@@ -0,0 +1,23 @@
+var canvas, ctx, _height, _width, frames = 0;
+
+function main() { // oh no is c language
+
+}
+
+function onClick() {
+
+}
+
+function setup() {
+
+}
+
+function update() {
+
+}
+
+function draw() {
+    
+}
+
+main();

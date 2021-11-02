f2ca46eeee5c757e3eb42982343459691b1ca4c7
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-01 23:54:24 -0300

    feat: player's jump

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let canvas, ctx, mainHeight = window.innerHeight, mainWidth = window.innerWidth, _frames = 0;
+const maxJumps = 3;
+
 
 class Element {
     constructor(x, y, height, width, color) {
@@ -16,22 +18,38 @@ class Element {
 }
 
 class Block extends Element {
-    constructor(x, y, height, width, color) {
+    constructor(x, y, height, width, color, gravity, speed, jumpForce) {
         super(x, y, height, width, color);
-        this.gravity = 1.5;
-        this.speed = 0;
+        this.gravity = gravity;
+        this.speed = speed;
+        this.jumpForce = jumpForce;
+        this.numJumps = 0;
     }
 
-    updateBlock() {
+    updateGravity() {
         this.speed += this.gravity;
         this.y += this.speed;
+    }
+
+    isGrounded(ground) {
+        if(this.y > ground.y - this.height) {
+            this.y = ground.y - this.height;
+            this.numJumps = 0;
+        }
+    }
+
+    jump() {
+        if(this.numJumps < maxJumps) {
+            this.speed = -this.jumpForce;
+            this.numJumps++;
+        }
     }
 }
 
 getMainDimensions();
 
 let ground = new Element(0, 550, 50, mainWidth, '#ffdf70');
-let player = new Block(50, 0, 50, 50, '#ff4e4e');
+let player = new Block(50, 0, 50, 50, '#ff4e4e', 1.5, 0, 15);
 
 /* All game functions */
 
@@ -53,6 +71,7 @@ function main() { // oh no is c language
 
 function onClick() {
     //alert('you clicked')
+    player.jump();
 }
 
 function getMainDimensions() {
@@ -74,7 +93,8 @@ function setup() {
 
 function update() {
     _frames++;
-    player.updateBlock();
+    player.updateGravity();
+    player.isGrounded(ground);
 }
 
 function draw() {

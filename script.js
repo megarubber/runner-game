fe6f427878eb1f9cb46f7823f3732c37efa5112a
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-01 22:53:14 -0300

    feat: creating the object 'ground'

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var canvas, ctx, _height, _width, frames = 0;
+var canvas, ctx, _height, _width, _frames = 0;
+
+ground = {
+    _y : 550,
+    _height : 50,
+    _color : '#ffdf70',
+
+    drawGround : function() {
+        ctx.fillStyle = this._color;
+        ctx.fillRect(0, this._y, _width, this._height);
+    }
+};
 
 function main() { // oh no is c language
     _height = window.innerHeight;
@@ -14,10 +25,12 @@ function main() { // oh no is c language
     canvas.height = _height;
     canvas.style.border = "1px solid #000";
 
-    ctx = canvas.getContext("2d");
+    ctx = canvas.getContext('2d');
     document.body.appendChild(canvas);
 
-    document.addEventListener("mousedown", onClick);
+    document.addEventListener('mousedown', onClick);
+
+    setup();
 }
 
 function onClick() {
@@ -25,15 +38,21 @@ function onClick() {
 }
 
 function setup() {
+    update();
+    draw();
 
-}
+    window.requestAnimationFrame(setup);
+}   
 
 function update() {
-
+    _frames++;
 }
 
 function draw() {
+    ctx.fillStyle = '#50beff';
+    ctx.fillRect(0, 0, _width, _height);
 
+    ground.drawGround();
 }
 
 main();

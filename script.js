18735c816abf0f97129cc3928315a053dd44afe4
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-01 22:36:08 -0300

    feat: style css

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var canvas, ctx, _height, _width, frames = 0;
 
 function main() { // oh no is c language
+    _height = window.innerHeight;
+    _width = window.innerWidth;
 
+    if(_width >= 500) {
+        _width = 600;
+        _height = 600;
+    }
+
+    canvas = document.createElement("canvas");
+    canvas.width = _width;
+    canvas.height = _height;
+    canvas.style.border = "1px solid #000";
 }
 
 function onClick() {
@@ -17,7 +28,7 @@ function update() {
 }
 
 function draw() {
-    
+
 }
 
 main();
